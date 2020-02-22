944eecf211dd348df4b4c78155cfa27190374d9d
BenStevens00 <59851958+BenStevens00@users.noreply.github.com>
2020-02-21 20:42:03 -0500

    E06 homework

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -2,8 +2,18 @@ extends Node2D
 
 onready var Camera = $Camera
 
+
+
 func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	$Camera.add_trauma(0.1)
+	if Input.is_action_pressed("left"):
+		Camera.add_trauma(1)
+	else:
+		pass
+
+	
+
+	
